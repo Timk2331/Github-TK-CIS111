18562760b99d86907f6bd00d5d5bd0774d46102a
Timk2331 <tkotov2331@students.mc3.edu>
2024-04-17 17:02:16 -0400

    HelloWorldKC11
    CIS111 hello world. This repeats hello world 100 times.

---
+++ HelloWorldKc11.java
@@ -0,0 +1,10 @@
+public class HelloWorldKc11 
+   {
+   public static void main(String[] args)
+   {
+      for (int i = 1; i <= 100; i++)
+      {
+         System.out.println("Hello World");
+    }
+    }
+}
